aa99a2299dd79e06d4a2671a360e360d1de59d5d
Federico Gonzalez <ae_fgonzalez@idcontractors.net>
2021-02-17 23:26:34 -0300

    Products

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,42 @@
 import styles from "../styles/Header.module.scss";
-import React from "react"
+import React from "react";
 
-
-function Header (){
-    return (
-        <>
-            <button className={styles.nav_toggle} aria-label="toggle navigation">
-                <span className={styles.hamburger}><i class="ri-menu-line"></i></span>
-            </button>
-            <h1>MÀLTTON</h1>
-            <button className={styles.carrito} aria-label="ir al carrito">
-                <i class="ri-shopping-bag-fill"></i>
-            </button>
-            <nav className="nav">
-                <ul className="nav__list">
-                    <li className="nav__item">
-                        {/* <a href="#" className="nav__link"></a> */}
-                    </li>
-                    <li className="nav__item">
-                        <a href="#inicio" className="nav__link">Inicio</a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="#productos" className="nav__link">Productos</a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="#contacto" className="nav__link">Contacto</a>
-                    </li>
-                </ul>
-            </nav>
-      </>
-    )
+function Header() {
+  return (
+    <>
+      <button className={styles.nav_toggle} aria-label="toggle navigation">
+        <span className={styles.hamburger}>
+          <i class="ri-menu-line"></i>
+        </span>
+      </button>
+      <h1>MÀLTTON</h1>
+      <button className={styles.carrito} aria-label="ir al carrito">
+        <i class="ri-shopping-bag-fill"></i>
+      </button>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            {/* <a href="#" className="nav__link"></a> */}
+          </li>
+          <li className="nav__item">
+            <a href="#inicio" className="nav__link">
+              Inicio
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#productos" className="nav__link">
+              Products
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#contacto" className="nav__link">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
-export default Header
+export default Header;
